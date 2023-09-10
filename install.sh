60bee1e1c4a5c4c015aca96a0e4d0f8348f1f7da
Andrés Court <andres@andrescourt.com>
2023-09-09 22:34:36 -0500

    Final macos install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
      sudo pacman -S alacritty neovim tmux
      yay -S ttf-font-nerd
@@ -6,10 +8,13 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
      ln -s ~/.dotfiles/.bashrc ~/.bashrc
 elif [[ "$OSTYPE" == "darwin"* ]]; then
      /bin/bash -c "$(curl fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+     brew tap homebrew/cask-fontsbrew tap homebrew/cask-fonts
      brew install --cask alacritty font-hack-nerd-font
      brew install neovim tmux
+     #ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 fi
 
+ln -s ~/.dotfiles/tmux/ ~/.config/tmux
 ln -s ~/.dotfiles/alacritty/ ~/.config/alacritty
 ln -s ~/.dotfiles/nvim/ ~/.config/nvim
-ln -s ~/.dotfiles/tmux/ ~/.config/tmux
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
