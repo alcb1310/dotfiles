d92e521ebb282e60ac63c4a64e599e36df3e02ad
Andres Court <andres@andrescourt.com>
2023-09-09 21:40:25 -0500

    Removed oh-my-bash from configuration files

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+     sudo pacman -S alacritty neovim tmux
+elif [[ "$OSTYPE" == "darwin" ]]; then
+     /bin/bash -c "$(curl fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+     brew install --cask alacritty
+     brew install neovim tmux
+fi
+
 ln -s ~/.dotfiles/alacritty/ ~/.config/alacritty
 ln -s ~/.dotfiles/nvim/ ~/.config/nvim
 ln -s ~/.dotfiles/tmux/ ~/.config/tmux
-ln -s ~/.dotfiles/.oh-my-bash/ ~/.oh-my-bash
-ln -s ~/.dotfiles/.bashrc/ ~/.bashrc
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+mv ~/.bashrc ~/.bashrc.old
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
