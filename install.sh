4eeb6ac0bd47623c5e47478167c9178df95d7b14
Andres Court <andres@andrescourt.com>
2023-09-09 21:21:21 -0500

    added install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+ln -s ~/.dotfiles/alacritty/ ~/.config/alacritty
+ln -s ~/.dotfiles/nvim/ ~/.config/nvim
+ln -s ~/.dotfiles/tmux/ ~/.config/tmux
+ln -s ~/.dotfiles/.oh-my-bash/ ~/.oh-my-bash
+ln -s ~/.dotfiles/.bashrc/ ~/.bashrc
