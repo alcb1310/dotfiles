04963267556692d688c5eaa4a8be6892c9a31ddc
Andres Court <andres@andrescourt.com>
2023-09-09 21:44:34 -0500

    Updated the install script to use bash in linux only

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
      sudo pacman -S alacritty neovim tmux
+     bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+     mv ~/.bashrc ~/.bashrc.old
+     ln -s ~/.dotfiles/.bashrc ~/.bashrc
 elif [[ "$OSTYPE" == "darwin" ]]; then
      /bin/bash -c "$(curl fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
      brew install --cask alacritty
@@ -9,7 +12,3 @@ fi
 ln -s ~/.dotfiles/alacritty/ ~/.config/alacritty
 ln -s ~/.dotfiles/nvim/ ~/.config/nvim
 ln -s ~/.dotfiles/tmux/ ~/.config/tmux
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-
-mv ~/.bashrc ~/.bashrc.old
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
