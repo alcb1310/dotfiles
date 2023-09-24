1b433354a7610028d4b64e7683b476b94146bdff
Andres Court <andres@andrescourt.com>
2023-09-23 20:34:17 -0500

    final config

--- nvim/after/plugin/lsp-zero.lua
+++ nvim/after/plugin/lsp-zero.lua
@@ -33,14 +33,14 @@ lsp.nvim_workspace()
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+     ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+     ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
      ['<CR>'] = cmp.mapping.confirm({ select = true }),
      ["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+-- cmp_mappings['<Tab>'] = nil
+-- cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
      mapping = cmp_mappings
