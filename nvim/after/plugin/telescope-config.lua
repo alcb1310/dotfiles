c4d09fc551d82fcf28f6799bfc20ac573cac3754
Andres Court <andres@andrescourt.com>
2023-10-01 17:16:45 -0500

    Give description to telescope remaps

--- nvim/after/plugin/telescope-config.lua
+++ nvim/after/plugin/telescope-config.lua
@@ -1,6 +1,8 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc="Find files", silent=true })
+vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc="Grep a text", silent=true })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc="List opened buffers", silent=true })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc="Help tags", silent=true })
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc="Key maps", silent=true })
+vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc="Git files", silent=true })
