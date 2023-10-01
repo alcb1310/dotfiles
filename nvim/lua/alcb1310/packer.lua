c8f6be584f9d4d7c6eb7361adf18b80cd722de20
Andres Court <andres@andrescourt.com>
2023-10-01 17:58:58 -0500

    Added lsp-zero support

--- nvim/lua/alcb1310/packer.lua
+++ nvim/lua/alcb1310/packer.lua
@@ -37,6 +37,29 @@ return require('packer').startup(function(use)
                'rcarriga/nvim-notify',
           }
     }
+     use {
+          'VonHeikemen/lsp-zero.nvim',
+          branch = 'v2.x',
+          requires = {
+               -- LSP Support
+               { 'neovim/nvim-lspconfig' },
+               { 'williamboman/mason.nvim' },
+               { 'williamboman/mason-lspconfig.nvim' },
+
+               -- Autocompletion
+               { 'hrsh7th/nvim-cmp' },
+               { 'hrsh7th/cmp-buffer' },
+               { 'hrsh7th/cmp-path' },
+               { 'saadparwaiz1/cmp_luasnip' },
+               { 'hrsh7th/cmp-nvim-lsp' },
+               { 'hrsh7th/cmp-nvim-lua' },
+
+               -- Snippets
+               { 'L3MON4D3/LuaSnip' },
+               { 'rafamadriz/friendly-snippets' },
+          }
+     }
+
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
