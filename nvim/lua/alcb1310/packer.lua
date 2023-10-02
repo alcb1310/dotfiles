bbbd9eefbd0aec5ea1f15b455c13ee0f8291ac32
Andres Court <andres@andrescourt.com>
2023-10-01 20:14:57 -0500

    Added a status line

--- nvim/lua/alcb1310/packer.lua
+++ nvim/lua/alcb1310/packer.lua
@@ -29,6 +29,10 @@ return require('packer').startup(function(use)
           -- or                            , branch = '0.1.x',
           requires = { { 'nvim-lua/plenary.nvim' } }
      }
+     use {
+          'nvim-lualine/lualine.nvim',
+          requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+     }
 
      use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
      use {
