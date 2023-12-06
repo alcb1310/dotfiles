9a0e97406830f8907a98384350e9c5c5a55e1ef0
Andres Court <andres@andrescourt.com>
2023-12-06 16:51:10 -0500

    Added a vim shortcut to change branches

--- nvim/lua/alcb1310/remap.lua
+++ nvim/lua/alcb1310/remap.lua
@@ -25,6 +25,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>vt", "<cmd>silent !tmux neww ~/.dotfiles/scripts/session-change.sh<CR>")
+vim.keymap.set("n", "<leader>gb", "<cmd>silent !tmux neww ~/.dotfiles/scripts/gbranch.sh<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
