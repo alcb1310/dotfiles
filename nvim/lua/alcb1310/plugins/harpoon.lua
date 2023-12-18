5b16d9cc469ca3291ca9ec0b4483a989daf37bd2
Andres Court <andres@andrescourt.com>
2023-12-18 08:29:28 -0500

    added harpoon

--- nvim/lua/alcb1310/plugins/harpoon.lua
+++ nvim/lua/alcb1310/plugins/harpoon.lua
@@ -1,10 +1,25 @@
 return {
      "theprimeagen/harpoon",
      branch = "harpoon2",
+     dependencies = {
+          "nvim-lua/plenary.nvim",
+     },
      config = function()
           local harpoon = require("harpoon")
-          -- local ui = require("harpoon.ui")
-          harpoon:setup()
+
+          local settings = {
+               save_on_toggle = true,
+               sync_on_ui_close = false,
+               key = function()
+                    return vim.loop.cwd()
+               end,
+          }
+
+          -- REQUIRED
+          harpoon:setup({
+               settings = settings,
+          })
+          -- REQUIRED
 
           vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
           vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
@@ -13,6 +28,9 @@ return {
           vim.keymap.set("n", "<leader>e", function() harpoon:list():select(2) end)
           vim.keymap.set("n", "<leader>y", function() harpoon:list():select(3) end)
           vim.keymap.set("n", "<leader>t", function() harpoon:list():select(4) end)
+
+          vim.keymap.set('n', '<leader>th', ':Telescope harpoon marks<cr>',
+               { desc = '[T]elescope [H]arpoon', silent = true })
      end,
      lazy = true,
      keys = {
