69d99300265fbb9dab86ae95102681a9a02ecc2f
Andres Court <andres@andrescourt.com>
2023-12-29 14:04:45 -0500

    added keymap for TelescopeTodo

--- nvim/lua/alcb1310/remaps.lua
+++ nvim/lua/alcb1310/remaps.lua
@@ -11,40 +11,42 @@ k("n", "<leader>pv", vim.cmd.Ex, { desc = "Open Netrw", silent = true })
 k({ "n", "v" }, "<leader>bc", "<ESC>:bd<CR>", { desc = "Close current buffer", silent = true })
 
 -- Moves the selected line up or down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line up", silent = true })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line down", silent = true })
+k("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line up", silent = true })
+k("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line down", silent = true })
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "G", "Gzzzv")
+k("n", "J", "mzJ`z")
+k("n", "<C-d>", "<C-d>zz")
+k("n", "<C-u>", "<C-u>zz")
+k("n", "n", "nzzzv")
+k("n", "N", "Nzzzv")
+k("n", "G", "Gzzzv")
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+k("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+k({ "n", "v" }, "<leader>y", [["+y]])
+k("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set("n", "<leader>vt", "<cmd>silent !tmux neww ~/.dotfiles/scripts/session-change.sh<CR>")
--- vim.keymap.set("n", "<leader>gb", "<cmd>silent !tmux neww ~/.dotfiles/scripts/gbranch.sh<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+k("n", "<leader>vt", "<cmd>silent !tmux neww ~/.dotfiles/scripts/session-change.sh<CR>")
+-- k("n", "<leader>gb", "<cmd>silent !tmux neww ~/.dotfiles/scripts/gbranch.sh<CR>")
+k("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/init.lua<CR>");
+k("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+k("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+k("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/init.lua<CR>");
 
-vim.keymap.set("n", "<leader><leader>", function()
+k("n", "<leader><leader>", function()
      vim.cmd("so")
 end)
 
 local lua_command = ":lua require('rest-nvim').run()<cr>"
 vim.api.nvim_set_keymap("n", "<leader>ut", lua_command, { silent = true })
 
-vim.keymap.set({ "n", "v", "i" }, "<A-v>", vim.cmd.vsplit, {})
-vim.keymap.set({ "n", "v", "i" }, "<A-s>", vim.cmd.split, {})
+k({ "n", "v", "i" }, "<A-v>", vim.cmd.vsplit, {})
+k({ "n", "v", "i" }, "<A-s>", vim.cmd.split, {})
 
-vim.keymap.set("n", "gh", "<cmd>diffget //2<cr>", { desc = "Works on conflict" })
-vim.keymap.set("n", "gj", "<cmd>diffget //3<cr>", { desc = "Works on conflict" })
+k("n", "gh", "<cmd>diffget //2<cr>", { desc = "Works on conflict" })
+k("n", "gj", "<cmd>diffget //3<cr>", { desc = "Works on conflict" })
+
+k("n", "<leader>tt", ":TodoTelescope<CR>", { desc = "Show TODOs", silent = true })
