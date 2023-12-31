2f696ba77b396730a4dba53d73b7de761c214280
Andres Court <andres@andrescourt.com>
2023-12-30 21:01:27 -0500

    FIX: Change lazy event for codeium to work

--- nvim/lua/alcb1310/plugins/codeium.lua
+++ nvim/lua/alcb1310/plugins/codeium.lua
@@ -1,6 +1,6 @@
 return {
      'Exafunction/codeium.vim',
-     event = 'InsertEnter',
+     event = 'BufEnter',
      config = function()
           -- Change '<C-g>' here to any keycode you like.
           vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
