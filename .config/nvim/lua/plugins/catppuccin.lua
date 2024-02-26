fb6eb865ad4478597a815450e981a6f3e337b3fe
Andres Court <andres@andrescourt.com>
2024-02-25 22:37:19 -0500

    Catppuccin color scheme

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,11 @@
+return {
+    "catppuccin/nvim",
+    name = "catpuccin",
+    priority = 1000,
+    config = function()
+        vim.cmd.colorscheme "catppuccin"
+
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end,
+}
