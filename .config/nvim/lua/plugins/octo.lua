187145b41ff2281ebaaeb983dc578f38322d8abf
Andres Court <andres@andrescourt.com>
2024-02-25 22:51:55 -0500

    Github integration using Octo

---
+++ .config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,19 @@
+local k = vim.keymap.set
+
+return {
+    "pwntester/octo.nvim",
+    requires = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("octo").setup()
+
+        k("n", "<leader>O", ":Octo actions<CR>", { silent = true })
+        k("n", "<leader>prl", ":Octo pr list<CR>", { silent = true })
+        k("n", "<leader>prc", ":Octo pr create<CR>", { silent = true })
+        k("n", "<leader>gil", ":Octo issue list<CR>", { silent = true })
+        k("n", "<leader>gic", ":Octo issue create<CR>", { silent = true })
+    end,
+}
