be5517b4fc1237e9c54cab1460e630945c64fcc5
Andres Court <andres@andrescourt.com>
2024-03-03 12:57:16 -0500

    added the Comment plugin

---
+++ .config/nvim/lua/plugins/Comment.lua
@@ -0,0 +1,12 @@
+return {
+	"numToStr/Comment.nvim",
+	opts = {
+		toggler = {
+			line = "gcc",
+			block = "gb",
+		},
+		ignore = "^$", -- ignore empty lines
+		padding = true,
+	},
+	lazy = false,
+}
