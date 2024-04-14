57c08b1a6aaab7b7ea6c0fff1ff380081f04e041
Andres Court <andres@andrescourt.com>
2024-04-13 23:12:42 -0500

    move the keymaps to the corresponding file

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,32 +1,27 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    lazy = false,
-    opts = {
-      defaults = {
-        layout_config = {
-          prompt_position = "top",
-        },
-      },
-    },
-    config = function(_, opts)
-      require("telescope").load_extension("ui-select")
-      require("telescope").setup(opts)
+    {
+        "nvim-telescope/telescope.nvim",
 
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-    end,
-    dependencies = {
-      {
-        "nvim-lua/plenary.nvim",
-      },
+        tag = "0.1.6",
+        lazy = false,
+        opts = {
+            defaults = {
+                layout_config = {
+                    prompt_position = "top",
+                },
+            },
+        },
+        config = function(_, opts)
+            require("telescope").load_extension("ui-select")
+            require("telescope").setup(opts)
+        end,
+        dependencies = {
+            {
+                "nvim-lua/plenary.nvim",
+            },
+        },
     },
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+    },
 }
