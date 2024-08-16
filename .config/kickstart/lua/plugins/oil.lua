328e84559cccf9834e521b4f88f59598d34a37e0
Andres Court <andres@andrescourt.com>
2024-08-15 19:15:26 -0500

    remove kickstart nvim

--- .config/kickstart/lua/plugins/oil.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"stevearc/oil.nvim",
-	opts = {},
-	-- Optional dependencies
-	keys = {
-		{ "-", "<cmd>Oil<CR>" },
-	},
-	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-}
